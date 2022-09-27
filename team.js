7cd580d4cf8a188d2e455e022dc13b670c944109
Alnis Smidchens <alnis.smidchens@gmail.com>
2022-09-27 16:23:40 -0700

    Roster update

--- team.js
+++ team.js
@@ -1,323 +1,328 @@
 var leadership = [
-    {
-        name: "Mark Polyakov",
-        roles: "President",
-        image: "mark-polyakov.png"
-    },
-    {
-        name: "Stephanie Geeson",
-        roles: "Vice-President",
-        image: "stephanie-geeson.png"
-    },
-    {
-        name: "Mai-Linh Tran",
-        roles: "HuskySat-2 Project Manager<br>Recruiting<br>Treasurer",
-        image: "mai-linh-tran.png"
-    },
-    {
-        name: "Jeff Sternberg",
-        roles: "Secretary",
-        image: "member-nopic.png"
-    },
-    {
-        name: "Allen Aby",
-        roles: "Software Lead",
-        image: "allen-aby.png"
-    },
-    {
-        name: "Andrew Buckingham",
-        roles: "Electronics Lead",
-        image: "andrew-buckingham.png"
-    },
-    {
-        name: "Alnis Smidchens",
-        roles: "Webmaster<br>Mechanical lead",
-        image: "alnis-smidchens.png"
-    },
-    {
-        name: "Karman Singh",
-        roles: "Marketing/PR Lead",
-        image: "karman-singh.png"
-    },
+  {
+    name: "Stephanie Geeson",
+    roles: "Vice President",
+    image: "stephanie-geeson.png"
+  },
+  {
+    name: "Grace Kelly",
+    roles: "Mechanical member",
+    image: "member-nopic.png"
+  },
+  {
+    name: "Mark Polyakov",
+    roles: "Advisor",
+    image: "mark-polyakov.png"
+  },
+  {
+    name: "Alnis Smidchens",
+    roles: "President<br>Webmaster",
+    image: "alnis-smidchens.png"
+  },
+  {
+    name: "Edward Zhang",
+    roles: "Recruiter<br>Marketing",
+    image: "edward-zhang.jpg"
+  },
 ];
 
 var members = [
-    {
-        name: "Srushti Adesara",
-        roles: "Software",
-        image: "srushti-adesara.jpg"
-    },
-    {
-        name: "Rudy Garcia",
-        roles: "Electronics",
-        image: "rudy-garcia.png"
-    },
-    {
-        name: "Karen Haining",
-        roles: "Software",
-        image: "member-nopic.png"
-    },
-    {
-        name: "Peter Hale",
-        roles: "Electronics",
-        image: "member-nopic.png"
-    },
-    {
-        name: "Joseph Iannone",
-        roles: "Propulsion",
-        image: "joseph-iannone.jpg"
-    },
-    {
-        name: "Grace Kelly",
-        roles: "Mechanical member",
-        image: "member-nopic.png"
-    },
-    {
-        name: "Samuel Lott",
-        roles: "Mechanical",
-        image: "member-nopic.png"
-    },
-    {
-        name: "Ian McLean",
-        roles: "Science & Operations",
-        image: "ian-mclean.jpg"
-    },
-    {
-        name: "Armand Mousavi",
-        roles: "Star tracking software",
-        image: "member-nopic.png"
-    },
-    {
-        name: "Tri Nguyen",
-        roles: "Software",
-        image: "tri-nguyen.jpg"
-    },
-    {
-        name: "Hangliang Ren (Harry)",
-        roles: "Software",
-        image: "member-nopic.png"
-    },
-    {
-        name: "Jacob Sawyer",
-        roles: "Mechanical member",
-        image: "jacob-sawyer.jpg"
-    },
-    {
-        name: "Austin Shaw",
-        roles: "Solar panels",
-        image: "member-nopic.png"
-    },
-    {
-        name: "Mitsuki Shimomura",
-        roles: "Mechanical",
-        image: "mitsuki-shimomura.jpg"
-    },
-    {
-        name: "Sebastian Soetomo",
-        roles: "Software",
-        image: "member-nopic.png"
-    },
-    {
-        name: "Lucas Swanson",
-        roles: "Science",
-        image: "lucas-swanson.jpg"
-    },
-    {
-        name: "Ryan Welch",
-        roles: "Reaction wheels",
-        image: "member-nopic.png"
-    },
-    {
-        name: "Vincent Wilson",
-        roles: "Software",
-        image: "vincent-wilson.jpg"
-    },
-    {
-        name: "Kenneth Yang",
-        roles: "Software",
-        image: "kenneth-yang.jpg"
-    },
-    {
-        name: "Edward Zhang",
-        roles: "Software",
-        image: "edward-zhang.jpg"
-    },
+  {
+    name: "Sathvik Chinta",
+    roles: "",
+    image: "member-nopic.png"
+  },
+  {
+    name: "Karen Haining",
+    roles: "Software",
+    image: "member-nopic.png"
+  },
+  {
+    name: "Peter Hale",
+    roles: "Electronics",
+    image: "member-nopic.png"
+  },
+  {
+    name: "Tri Nguyen",
+    roles: "Software",
+    image: "tri-nguyen.jpg"
+  },
+  {
+    name: "Austin Shaw",
+    roles: "Solar panels",
+    image: "member-nopic.png"
+  },
+  {
+    name: "Mitsuki Shimomura",
+    roles: "Mechanical",
+    image: "mitsuki-shimomura.jpg"
+  },
+  {
+    name: "Ryan Welch",
+    roles: "Reaction wheels",
+    image: "member-nopic.png"
+  },
+  {
+    name: "Kenneth Yang",
+    roles: "Software",
+    image: "kenneth-yang.jpg"
+  },
+
 ];
 
 var alumni = [
-    {
-        name: "Austin Burnham",
-        roles: "Webmaster",
-        image: "austin-burnham.jpg"
-    },
-    {
-        name: "Sathvik Chinta",
-        roles: "",
-        image: "member-nopic.png"
-    },
-    {
-        name: "John Correy",
-        roles: "",
-        image: "john-correy.png"
-    },
-    {
-        name: "Jade Cutter",
-        roles: "Secretary<br>MiniMEE",
-        image: "jade-cutter.png"
-    },
-    {
-        name: "Sebastian Davis",
-        roles: "",
-        image: "sebastian-davis.png"
-    },
-    {
-        name: "Simon Fraser",
-        roles: "",
-        image: "member-nopic.png"
-    },
-    {
-        name: "Hans Gaensbauer",
-        roles: "",
-        image: "hans-gaensbauer.png"
-    },
-    {
-        name: "Jevin Gu",
-        roles: "",
-        image: "jevin-gu.png"
-    },
-    {
-        name: "Rahul Gupta",
-        roles: "",
-        image: "member-nopic.png"
-    },
-    {
-        name: "Jordan Hatch",
-        roles: "",
-        image: "jordan-hatch.png"
-    },
-    {
-        name: "Alexander Hicker",
-        roles: "",
-        image: "member-nopic.png"
-    },
-    {
-        name: "Logan Ince",
-        roles: "",
-        image: "logan-ince.png"
-    },
-    {
-        name: "Haili Kuester",
-        roles: "",
-        image: "member-nopic.png"
-    },
-    {
-        name: "Marshall Lee",
-        roles: "Mechanical Lead<br>MiniMEE",
-        image: "marshall-lee.png"
-    },
-    // {
-    //     name: "Katharine Lundblad",
-    //     roles: "",
-    //     image: "katharine-lundblad.png"
-    // },
-    {
-        name: "Richard Lee",
-        roles: "",
-        image: "member-nopic.png"
-    },
-    {
-        name: "John Leitch",
-        roles: "",
-        image: "member-nopic.png"
-    },
-    {
-        name: "Daniel Lord",
-        roles: "",
-        image: "member-nopic.png"
-    },
-    {
-        name: "Brett Lum",
-        roles: "",
-        image: "brett-lum.png"
-    },
-    {
-        name: "Logan McKinney",
-        roles: "",
-        image: "member-nopic.png"
-    },
-    {
-        name: "Teerth Mehta",
-        roles: "",
-        image: "member-nopic.png"
-    },
-    {
-        name: "Chase Mersberg",
-        roles: "",
-        image: "member-nopic.png"
-    },
-    {
-        name: "Walid Mouss",
-        roles: "",
-        image: "walid-mouss.png"
-    },
-    {
-        name: "Shulong Mo",
-        roles: "",
-        image: "member-nopic.png"
-    },
-    {
-        name: "James Ott",
-        roles: "Propulsion",
-        image: "james-ott.png"
-    },
-    {
-        name: "Kavel Rao",
-        roles: "",
-        image: "kavel-rao.png"
-    },
-    {
-        name: "Jared Rapp",
-        roles: "",
-        image: "jared-rapp.png"
-    },
-    {
-        name: "Eli Reed",
-        roles: "Former President",
-        image: "eli-reed.jpg"
-    },
-    {
-        name: "Seth Reed",
-        roles: "",
-        image: "member-nopic.png"
-    },
-    {
-        name: "Simon Shimel",
-        roles: "",
-        image: "simon-shimel.png"
-    },
-    {
-        name: "Andrew Smith",
-        roles: "",
-        image: "member-nopic.png"
-    },
-    {
-        name: "Haley Stewart",
-        roles: "",
-        image: "haley-stewart.png"
-    },
-    {
-        name: "Maia Willebrand",
-        roles: "",
-        image: "maia-willebrand.png"
-    },
+  {
+    name: "Srushti Adesara",
+    roles: "Software",
+    image: "srushti-adesara.jpg"
+  },
+  {
+    name: "Allen Aby",
+    roles: "Software Lead",
+    image: "allen-aby.png"
+  },
+  {
+    name: "Andrew Buckingham",
+    roles: "Electronics Lead",
+    image: "andrew-buckingham.png"
+  },
+  {
+    name: "Austin Burnham",
+    roles: "Webmaster",
+    image: "austin-burnham.jpg"
+  },
+  {
+    name: "John Correy",
+    roles: "",
+    image: "john-correy.png"
+  },
+  {
+    name: "Jade Cutter",
+    roles: "Secretary<br>MiniMEE",
+    image: "jade-cutter.png"
+  },
+  {
+    name: "Sebastian Davis",
+    roles: "",
+    image: "sebastian-davis.png"
+  },
+  {
+    name: "Simon Fraser",
+    roles: "",
+    image: "member-nopic.png"
+  },
+  {
+    name: "Hans Gaensbauer",
+    roles: "",
+    image: "hans-gaensbauer.png"
+  },
+  {
+    name: "Jevin Gu",
+    roles: "",
+    image: "jevin-gu.png"
+  },
+  {
+    name: "Rahul Gupta",
+    roles: "",
+    image: "member-nopic.png"
+  },
+  {
+    name: "Jordan Hatch",
+    roles: "",
+    image: "jordan-hatch.png"
+  },
+  {
+    name: "Rudy Garcia",
+    roles: "Electronics",
+    image: "rudy-garcia.png"
+  },
+  {
+    name: "Joseph Iannone",
+    roles: "Propulsion",
+    image: "joseph-iannone.jpg"
+  },
+  {
+    name: "Samuel Lott",
+    roles: "Mechanical",
+    image: "member-nopic.png"
+  },
+  {
+    name: "Ian McLean",
+    roles: "Science & Operations",
+    image: "ian-mclean.jpg"
+  },
+  {
+    name: "Armand Mousavi",
+    roles: "Star tracking software",
+    image: "member-nopic.png"
+  },
+  {
+    name: "Hangliang Ren (Harry)",
+    roles: "Software",
+    image: "member-nopic.png"
+  },
+  {
+    name: "Jacob Sawyer",
+    roles: "Mechanical member",
+    image: "jacob-sawyer.jpg"
+  },
+
+
+  {
+    name: "Sebastian Soetomo",
+    roles: "Software",
+    image: "member-nopic.png"
+  },
+  {
+    name: "Lucas Swanson",
+    roles: "Science",
+    image: "lucas-swanson.jpg"
+  },
+
+  {
+    name: "Vincent Wilson",
+    roles: "Software",
+    image: "vincent-wilson.jpg"
+  },
+  {
+    name: "Alexander Hicker",
+    roles: "",
+    image: "member-nopic.png"
+  },
+  {
+    name: "Logan Ince",
+    roles: "",
+    image: "logan-ince.png"
+  },
+  {
+    name: "Haili Kuester",
+    roles: "",
+    image: "member-nopic.png"
+  },
+  {
+    name: "Marshall Lee",
+    roles: "Mechanical Lead<br>MiniMEE",
+    image: "marshall-lee.png"
+  },
+  // {
+  //     name: "Katharine Lundblad",
+  //     roles: "",
+  //     image: "katharine-lundblad.png"
+  // },
+  {
+    name: "Richard Lee",
+    roles: "",
+    image: "member-nopic.png"
+  },
+  {
+    name: "John Leitch",
+    roles: "",
+    image: "member-nopic.png"
+  },
+  {
+    name: "Daniel Lord",
+    roles: "",
+    image: "member-nopic.png"
+  },
+  {
+    name: "Brett Lum",
+    roles: "",
+    image: "brett-lum.png"
+  },
+  {
+    name: "Logan McKinney",
+    roles: "",
+    image: "member-nopic.png"
+  },
+  {
+    name: "Teerth Mehta",
+    roles: "",
+    image: "member-nopic.png"
+  },
+  {
+    name: "Chase Mersberg",
+    roles: "",
+    image: "member-nopic.png"
+  },
+  {
+    name: "Walid Mouss",
+    roles: "",
+    image: "walid-mouss.png"
+  },
+  {
+    name: "Shulong Mo",
+    roles: "",
+    image: "member-nopic.png"
+  },
+  {
+    name: "James Ott",
+    roles: "Propulsion",
+    image: "james-ott.png"
+  },
+
+  {
+    name: "Kavel Rao",
+    roles: "",
+    image: "kavel-rao.png"
+  },
+  {
+    name: "Jared Rapp",
+    roles: "",
+    image: "jared-rapp.png"
+  },
+  {
+    name: "Eli Reed",
+    roles: "Former President",
+    image: "eli-reed.jpg"
+  },
+  {
+    name: "Seth Reed",
+    roles: "",
+    image: "member-nopic.png"
+  },
+  {
+    name: "Simon Shimel",
+    roles: "",
+    image: "simon-shimel.png"
+  },
+  {
+    name: "Karman Singh",
+    roles: "Marketing/PR Lead",
+    image: "karman-singh.png"
+  },
+  {
+    name: "Andrew Smith",
+    roles: "",
+    image: "member-nopic.png"
+  },
+  {
+    name: "Jeff Sternberg",
+    roles: "Secretary",
+    image: "member-nopic.png"
+  },
+  {
+    name: "Haley Stewart",
+    roles: "",
+    image: "haley-stewart.png"
+  },
+  {
+    name: "Mai-Linh Tran",
+    roles: "HuskySat-2 Project Manager<br>Recruiting<br>Treasurer",
+    image: "mai-linh-tran.png"
+  },
+  {
+    name: "Maia Willebrand",
+    roles: "",
+    image: "maia-willebrand.png"
+  },
 ];
 
 var cards = {
-    leadership, members, alumni
+  leadership, members, alumni
 }[document.currentScript.getAttribute('team')];
 
 cards.forEach(member => document.write(
-    `
+  `
 <div class="team-member-card">
     <img width=200px height=200px class="team-member-photo" src="images/members/${member.image}">
     <p class="team-member-name">${member.name}</p>
