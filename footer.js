012133deee564af3e0af5f357ef57730586f608f
manforowicz <manforowicz@gmail.com>
2023-09-08 17:13:10 -0700

    Added netlify logo to footer

--- footer.js
+++ footer.js
@@ -1,30 +1,38 @@
 document.write(
 `
 <div class="footer">
-    <div class="footer-element">
-        <p class="footer-column-header">Connect with us</p>
-        <a class="footer-link" href="mailto:cubesat@uw.edu">cubesat@uw.edu</a><br>
-        <a class="footer-link" href="https://www.facebook.com/huskysat/">Facebook</a><br>
-        <a class="footer-link" href="https://twitter.com/huskysat">Twitter</a><br>
-        <a class="footer-link" href="https://github.com/UWCubeSat">GitHub</a><br>
-        <a class="footer-link" href="https://www.instagram.com/huskysatlab/">Instagram</a><br>
-        <a class="footer-link" href="https://huskylink.washington.edu/organization/cubesat">HuskyLink</a><br>
-        <a class="footer-link" href="https://discord.gg/GFjsq4Y">Discord</a><br>
-        <a class="footer-link" href="http://huskysatellitelab.com">Old website</a><br>
-        <a class="footer-link" href="https://newsletter.huskysat.org">Newsletter</a>
-    </div>
+    <div class="footer-content">
+        <div class="footer-element">
+            <p class="footer-column-header">Connect with us</p>
+            <a class="footer-link" href="mailto:cubesat@uw.edu">cubesat@uw.edu</a><br>
+            <a class="footer-link" href="https://www.facebook.com/huskysat/">Facebook</a><br>
+            <a class="footer-link" href="https://twitter.com/huskysat">Twitter</a><br>
+            <a class="footer-link" href="https://github.com/UWCubeSat">GitHub</a><br>
+            <a class="footer-link" href="https://www.instagram.com/huskysatlab/">Instagram</a><br>
+            <a class="footer-link" href="https://huskylink.washington.edu/organization/cubesat">HuskyLink</a><br>
+            <a class="footer-link" href="https://discord.gg/GFjsq4Y">Discord</a><br>
+            <a class="footer-link" href="http://huskysatellitelab.com">Old website</a><br>
+            <a class="footer-link" href="https://newsletter.huskysat.org">Newsletter</a>
+        </div>
 
-    <div class="footer-element">
-        <p class="footer-column-header">Missions</p>
-        <a class="footer-link" href="missions.html#huskysat-1">HuskySat-1</a><br>
-        <a class="footer-link" href="missions.html#phat-1">PHAT-1</a><br>
-        <a class="footer-link" href="missions.html#phat-2">PHAT-2</a><br>
-        <a class="footer-link" href="missions.html#minimee">MiniMee</a><br>
-        <a class="footer-link" href="missions.html#huskysat-2">HuskySat-2</a><br>
-        <p class="footer-column-header">Join us</p>
-        <a class="footer-link" href="join.html#student">Apply for membership</a><br>
-        <a class="footer-link" href="join.html#sponsor">Sponsor us</a><br>
+        <div class="footer-element">
+            <p class="footer-column-header">Missions</p>
+            <a class="footer-link" href="missions.html#huskysat-1">HuskySat-1</a><br>
+            <a class="footer-link" href="missions.html#phat-1">PHAT-1</a><br>
+            <a class="footer-link" href="missions.html#phat-2">PHAT-2</a><br>
+            <a class="footer-link" href="missions.html#minimee">MiniMee</a><br>
+            <a class="footer-link" href="missions.html#huskysat-2">HuskySat-2</a><br>
+            <p class="footer-column-header">Join us</p>
+            <a class="footer-link" href="join.html#student">Apply for membership</a><br>
+            <a class="footer-link" href="join.html#sponsor">Sponsor us</a><br>
+        </div>
+
+
+    </div>
+    <div class="netlify-logo">
+        <a href="https://www.netlify.com"> <img src="https://www.netlify.com/v3/img/components/netlify-color-accent.svg" alt="Deploys by Netlify" /> </a>
     </div>
 </div>
+
 `
 );
