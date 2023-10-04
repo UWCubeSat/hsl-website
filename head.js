83866d7d35b8e626f9c96612ab5c06de0b82f73d
Marcin Anforowicz <manforowicz@gmail.com>
2023-10-04 13:05:48 -0700

    Better method

--- head.js
+++ head.js
@@ -8,5 +8,6 @@ document.write(
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Titillium+Web:ital,wght@0,300;0,600;0,700;1,300&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
 `
 );
